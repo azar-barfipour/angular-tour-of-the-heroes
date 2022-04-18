12962d9f6a333b2f520822198b6c994547b16875
Azar Barfipour <azar.barfipour@gmail.com>
2022-04-18 15:05:13 -0700

    added a message for fetching data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,12 @@ import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgModel lives here
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 // for http request
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
-import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -9,5 +9,7 @@ import { MessageService } from '../message.service';
 export class MessagesComponent implements OnInit {
   constructor(public messageService: MessageService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.messageService.add('Data fetched successfully');
+  }
 }
